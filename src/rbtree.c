b6bc859a73e5a8fec3acce24081bee705e9f6e22
uujeen <rkddmlwls2@gmail.com>
2023-05-21 17:25:54 +0900

    rbtree add comment

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,34 +1,39 @@
 #include "rbtree.h"
 #include <stdlib.h>
-
+/**
+ * CLRS Red-Black Tree
+ * Red-Black Tree C언어로 구현하기.
+*/
 rbtree *new_rbtree(void) {
   // TODO: initialize struct if needed
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   
-  p->nil = (node_t *)calloc(1, sizeof(node_t));
-  p->nil->color = RBTREE_BLACK;
-  p->root = p->nil;
+  p->nil = (node_t *)calloc(1, sizeof(node_t)); // nil 노드에 노드를 구현
+  p->nil->color = RBTREE_BLACK; // 조건에 따라 nil 노드의 색상을 블랙으로 표시
+  p->root = p->nil; // 초기 상태 root 노드를 nil 노드와 같은 형태로 선언 (색상과 노드의 구성 (색상, 키값, 부모 자식노드))
 
   return p;
 }
 
 void post_delete(node_t *root, node_t *nil){
   if(root == NULL || root == nil) return;
-  post_delete(root->left, nil);
-  post_delete(root->right, nil);
+  post_delete(root->left, nil); // 왼쪽
+  post_delete(root->right, nil); // 오른쪽
   if(root != nil) {
-    free(root);
+    free(root); // 부모 노드 삭제
+  }
+  else {
+    free(root); // nil 노드 삭제
   }
 }
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
-  post_delete(t->root, t->nil);
-  free(t->nil);
-  free(t);
+  post_delete(t->root, t->nil); // 후위순회방식으로 트리의 모든 노드 반환
+  free(t); // 트리 반환
 }
 
-void left_rotate(rbtree *t, node_t *x){
+void left_rotate(rbtree *t, node_t *x){ // 왼쪽방향으로 트리 회전
   node_t* y = x->right;
   x->right = y->left;
   if(y->left != t->nil) {
@@ -48,7 +53,7 @@ void left_rotate(rbtree *t, node_t *x){
   x->parent = y;
 }
 
-void right_rotate(rbtree *t, node_t *x){
+void right_rotate(rbtree *t, node_t *x){ // 오른쪽 방향으로 트리 회전
   node_t* y = x->left;
   x->left = y->right;
   if(y->right != t->nil){
@@ -137,12 +142,12 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
   }
   z->left = t->nil;
   z->right = t->nil;
-  z->color = RBTREE_RED;
-  rbtree_insert_fixup(t, z);
+  z->color = RBTREE_RED; // 삽입시 항상 레드 색상
+  rbtree_insert_fixup(t, z); // 삽입 후 색상와 위치를 수정한다.
   return z;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
+node_t *rbtree_find(const rbtree *t, const key_t key) { // 이진탐색트리의 find와 동일한 방식
   // TODO: implement find
   node_t* v;
   v = t->root;
@@ -255,35 +260,35 @@ void rbtree_delete_fixup(rbtree* t, node_t* x) {
   x->color = RBTREE_BLACK;
 }
 
-node_t* rbtree_minimum(node_t *nil, node_t* x) {  
+node_t* rbtree_minimum(node_t *nil, node_t* x) { // 삭제 연산 시 오른쪽 서브트리의 가장 작은 값을 반환
   while(x->left != nil){
     x = x->left;
   }
   return x;
 }
 
-int rbtree_erase(rbtree *t, node_t *p) {
+int rbtree_erase(rbtree *t, node_t *p) { // 삭제연산
   // TODO: implement erase
   node_t* y = p;
   node_t* x;
   color_t y_original_color = y->color;
   if(p->left == t->nil) {
     x = p->right;
-    rbtree_transplant(t, p, p->right);
+    rbtree_transplant(t, p, x); // 서브트리를 옮기는 함수
   }
   else if(p->right == t->nil) {
     x = p->left;
-    rbtree_transplant(t, p, p->left);
+    rbtree_transplant(t, p, x);
   }
   else {
-    y = rbtree_minimum(t->nil, p->right);
+    y = rbtree_minimum(t->nil, p->right); // 대체할 노드를 찾을 때 오른쪽 서브트리의 가장 작은 수를 찾는다. 동일하게 왼쪽 서브트리의 가장 큰 값을 찾는 방법도 있다.
     y_original_color = y->color;
     x = y->right;
     if(y->parent == p) {
       x->parent = y;
     }
     else {
-      rbtree_transplant(t, y, y->right);
+      rbtree_transplant(t, y, x);
       y->right = p->right;
       y->right->parent = y;
     }
@@ -295,12 +300,8 @@ int rbtree_erase(rbtree *t, node_t *p) {
   free(p);
   p = NULL;
   if(y_original_color == RBTREE_BLACK) {
-    rbtree_delete_fixup(t, x);
-  }  
-  t->nil->parent = NULL;
-  t->nil->right = NULL;
-  t->nil->left = NULL;
-  t->nil->color = RBTREE_BLACK;
+    rbtree_delete_fixup(t, x); // 삭제 후 색상와 위치를 수정한다.
+  } 
   return 0;
 }
 
@@ -314,7 +315,7 @@ void in_order(node_t *root, node_t *nil, key_t *arr, int* index){
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
   int *index = calloc(1, n);
-  in_order(t->root, t->nil, arr, index);
+  in_order(t->root, t->nil, arr, index); // 전위순회 방식으로 배열로 저장하면 가장 작은값부터 오름차순으로 저장
   free(index);
   return 0;
 }
--- src/rbtree.h
+++ src/rbtree.h
@@ -3,17 +3,17 @@
 
 #include <stddef.h>
 
-typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+typedef enum { RBTREE_RED, RBTREE_BLACK } color_t; // 0 : Red, 1 : BLACK
 
 typedef int key_t;
 
-typedef struct node_t {
+typedef struct node_t { // 노드의 구성 : 색상, 키, 부모 자식 노드
   color_t color;
   key_t key;
   struct node_t *parent, *left, *right;
 } node_t;
 
-typedef struct {
+typedef struct { // 트리의 초기구조 root, nil노드
   node_t *root;
   node_t *nil;  // for sentinel
 } rbtree;
